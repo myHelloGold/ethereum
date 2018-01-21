064d1d9c967458268620a4953d3a325a0d077af4
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-01-21 14:25:37 +0000

    Add Geth starter file for AWS EC2

---
+++ scripts/start-geth-aws.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+me="$(whoami)"
+geth_ps_owner="$(ps -C geth -o ruser= | grep -w $me)"
+
+cmd="nohup geth --nodiscover --fast --cache=512 --rpc --rpcapi='db,eth,net,web3,personal,admin' --mine --minerthreads=1 --ws --wsapi='db,eth,net,web3,personal,admin' --wsorigins='*' --verbosity='0' &>/dev/null &"
+
+if [ -z "$geth_ps_owner" ]
+then
+  eval $cmd
+  echo "Geth is running"
+else
+  echo "Geth already running"
+  exit 1
+fi
